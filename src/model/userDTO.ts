4e0860c4c83ba22fad7c2058b399895ace5f815b
Douglas Williams <douglaswilliams298@gmail.com>
2023-02-17 21:04:10 -0300

    Feat: Se aplicado esse commit comecei o trabalho, instalei as dependências, criei as pastas e os arquivos relacionados ao usuário

---
+++ src/model/userDTO.ts
@@ -0,0 +1,21 @@
+export interface UserInputDTO { //Para cadastrar um usuário
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface LoginInputDTO { //Para logar um usuário
+    email: string;
+    password: string;
+}
+
+export interface CreateUser { //Para criar um usuário
+    id: string,
+    name: string,
+    email: string,
+    password: string
+ }
+
+ export interface UserIdDTO { //Para pegar o id do usuário
+    id: string
+}
